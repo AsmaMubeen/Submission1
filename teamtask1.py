212026134f4efdeed41ab7292d7dd4639364350d
Asma Mubeen <amubeen03asma@gmail.com>
2018-01-06 21:32:36 +0530

    First commit

---
+++ teamtask1.py
@@ -0,0 +1,38 @@
+from wit import Wit
+import requests
+from flask import Flask, request, redirect, render_template, make_response, abort, jsonify
+import json
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+
+
+access_token = os.environ.get("ACCESSTOKEN")
+
+client = Wit(access_token = access_token)
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST','GET'])
+def index(arg = None):
+	if request.method == 'GET':
+		return render_template("index.html",arg = arg)
+	elif request.method == 'POST':
+		message_text = request.form['input']
+		response = client.message(message_text)
+		respList = []
+		
+		for key, val in response['entities'].items():
+			if len(val)>0:
+				respList.append({'entityType':key,'entityValue': val[0]['value']})
+		
+		return json.dumps(respList)
+
+
+if __name__ == "__main__":
+	app.run()
